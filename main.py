7a3a7c4164bff337f37dfb9900af10d3cf053fd2
Ilya484 <ucdo854@kemcdo.ru>
2021-09-03 12:37:59 +0700

    add iclass.py

--- main.py
+++ main.py
@@ -1,44 +1,5 @@
 from config_window import *
-from PIL import ImageTk, Image
-
-from tkinter import Tk, Label, Button
-
-
-class Window:
-    def __init__(self, width, heigth, tranform, xpos, ypos, title, logo):
-        self.width = width
-        self.heigth = heigth
-        self.tranform = tranform
-        self.xpos = xpos
-        self.ypos = ypos
-        self.title = title
-        self.logo = logo
-        self.root = Tk()
-    
-    def build(self):
-        self.root.geometry(f"{self.width}x{self.heigth}"
-                           f"+{self.xpos}+{self.ypos}")
-        self.root.resizable(*self.tranform)
-        self.root.iconbitmap(default=self.logo)
-        self.root.title(self.title)
-    
-    def run(self):
-        self.root.mainloop()
-    
-    def set_text(self, txt, fg, position, font_param, bg=None):
-        Label(self.root, text=txt, background=bg, foreground=fg, font=font_param).place(x=position[0],
-                                                                                        y=position[1])
-    
-    def set_image(self, image, size, position):
-        global tkimage
-        img = Image.open(image)
-        img = img.resize(size)
-        tkimage = ImageTk.PhotoImage(img)
-        Label(self.root, image=tkimage).place(x=position[0], y=position[1])
-    
-    def set_button(self, txt, font_param, position, event=None, background=None):
-        Button(self.root, text=txt, bg=background, command=event, font=font_param).place(x=position[0],
-                                                                                         y=position[1])
+from iclass import Window
 
 
 if __name__ == "__main__":
