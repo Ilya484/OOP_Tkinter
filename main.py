1977783fc3f0dcc2d4d9ffddc2d77676223a4250
Ilya484 <ucdo854@kemcdo.ru>
2021-09-03 19:51:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
     wind.build()
     wind.set_image(image="resources/pig.jpg", size=(256, 256), position=(0, 500))
     wind.set_text(txt="HELLO", fg='black', position=(0, 20), font=("Arial", 20))
-    wind.set_button(txt="click", background='red', position=(0, 50), font=("Arial Black", 12), event=lambda: print('click'))
+    wind.set_button(txt="click", background='red', position=(0, 50), font=("Arial", 12), event=lambda: print('click'))
     wind.run()
